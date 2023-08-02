19e56771be2c910f7d22b29f940105f8ca45d83f
langxing6 <2797059099@qq.com>
2023-08-03 01:45:51 +0800

    0803 update code and PPT.

--- src/main/java/com/example/server_i18n/controller/TestControler.java
+++ src/main/java/com/example/server_i18n/controller/TestControler.java
@@ -1,35 +1,26 @@
 package com.example.server_i18n.controller;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 @RestController
 @RequestMapping("/test")
 public class TestControler {
 
     @Autowired
-    private MessageSource messageSource; // setDefaultEncoding
-
-    @Autowired
-    private HttpServletRequest request;
-
+    private MessageSource messageSource;
 
     @GetMapping("/hello")
-    public String hello() {
-        System.out.println(LocaleContextHolder.getLocale());
-        String message = messageSource.getMessage("FileNotExistErrorMessage", null, LocaleContextHolder.getLocale());
+    public String hello(@RequestParam(value="key") String key) {
 
-        return message;
+        String message = messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
+
+        return LocaleContextHolder.getLocale() + " : " + message;
     }
 
 }
